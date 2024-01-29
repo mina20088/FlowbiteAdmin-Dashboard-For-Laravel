cb60a6538743dce71cdc90cf1f300b56cf9e7b59
minaremon <minaremonshaker@gmail.com>
2024-01-29 18:28:19 +0200

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   resources/js/app.js 	modified:   resources/views/components/layouts/app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,14 +12,14 @@
     <meta name="theme-color" content="#ffffff">
     <title>Tailwind CSS Dashboard Stacked Layout - Flowbite</title>
 
-    @vite(['resources/css/app.css','resources/js/app.js'],null, ['defer'=>true])
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body  class="bg-gray-50 dark:bg-gray-800">
 
     @include('partials.__header')
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <script src="https://flowbite-admin-dashboard.vercel.app//app.bundle.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/datepicker.min.js"></script>
+    <script async defer src="https://buttons.github.io/buttons.js" defer></script>
+    <script src="https://flowbite-admin-dashboard.vercel.app//app.bundle.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/datepicker.min.js" defer></script>
 </body>
 
 </html>
