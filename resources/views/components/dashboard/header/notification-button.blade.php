cd0cbb9d833f84061f705e83a025d9e53c3b0e18
minaremon <minaremonshaker@gmail.com>
2024-02-04 21:17:53 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   resources/views/components/dashboard/header/apps-button.blade.php
    	modified:   resources/views/components/dashboard/header/apps-drop-down.blade.php
    	modified:   resources/views/components/dashboard/header/dark-white-mode-toggler.blade.php
    	modified:   resources/views/components/dashboard/header/mobile-search.blade.php
    	modified:   resources/views/components/dashboard/header/notification-button.blade.php
    	new file:   resources/views/components/dashboard/sidebar/partials/__sidebar-countrys-dropdown.blade.php
    	new file:   resources/views/components/dashboard/sidebar/partials/__sidebar-fotter.blade.php
    	new file:   resources/views/components/dashboard/tooltip.blade.php
    	new file:   resources/views/components/s-v-g-s/app.blade.php
    	new file:   resources/views/components/s-v-g-s/bill.blade.php
    	new file:   resources/views/components/s-v-g-s/billing.blade.php
    	new file:   resources/views/components/s-v-g-s/inbox.blade.php
    	new file:   resources/views/components/s-v-g-s/logout.blade.php
    	new file:   resources/views/components/s-v-g-s/mode.blade.php
    	new file:   resources/views/components/s-v-g-s/pricing.blade.php
    	new file:   resources/views/components/s-v-g-s/products.blade.php
    	new file:   resources/views/components/s-v-g-s/profile.blade.php
    	new file:   resources/views/components/s-v-g-s/sales.blade.php
    	new file:   resources/views/components/s-v-g-s/search.blade.php
    	new file:   resources/views/components/s-v-g-s/settings.blade.php
    	new file:   resources/views/components/s-v-g-s/users.blade.php
    	modified:   resources/views/partials/__sidebar.blade.php

--- resources/views/components/dashboard/header/notification-button.blade.php
+++ resources/views/components/dashboard/header/notification-button.blade.php
@@ -1,5 +1,5 @@
 <button type="button" data-dropdown-toggle="notification-dropdown" class="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">
     <span class="sr-only">View notifications</span>
     <!-- Bell icon -->
-    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path></svg>
+    <x-s-v-g-s.bill class="w-6 h-6"/>
   </button>
