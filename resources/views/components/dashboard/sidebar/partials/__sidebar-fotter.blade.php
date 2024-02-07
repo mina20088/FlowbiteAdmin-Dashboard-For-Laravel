ec93d343247ec2272919b26884c1451b78f8e4d1
mina remon <minaremonshaker@gmail.com>
2024-02-07 19:01:32 +0200

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   .timetracker
    	modified:   resources/views/components/dashboard/header/notification-drop-down-items.blade.php
    	modified:   resources/views/components/dashboard/header/notification-drop-down.blade.php
    	modified:   resources/views/components/dashboard/header/search-form.blade.php
    	modified:   resources/views/components/dashboard/header/sidebar-toggler-button.blade.php
    	modified:   resources/views/components/dashboard/sidebar/drop-down-button.blade.php
    	modified:   resources/views/components/dashboard/sidebar/mobile-search-form.blade.php
    	modified:   resources/views/components/dashboard/sidebar/partials/__sidebar-fotter.blade.php
    	new file:   resources/views/components/s-v-g-s/arrow-down.blade.php
    	new file:   resources/views/components/s-v-g-s/camera.blade.php
    	new file:   resources/views/components/s-v-g-s/close.blade.php
    	new file:   resources/views/components/s-v-g-s/comment.blade.php
    	new file:   resources/views/components/s-v-g-s/crud.blade.php
    	new file:   resources/views/components/s-v-g-s/dashboard.blade.php
    	new file:   resources/views/components/s-v-g-s/eye.blade.php
    	new file:   resources/views/components/s-v-g-s/folow.blade.php
    	new file:   resources/views/components/s-v-g-s/hamburger.blade.php
    	new file:   resources/views/components/s-v-g-s/hart.blade.php
    	new file:   resources/views/components/s-v-g-s/layouts.blade.php
    	new file:   resources/views/components/s-v-g-s/lock.blade.php
    	new file:   resources/views/components/s-v-g-s/messages.blade.php
    	new file:   resources/views/components/s-v-g-s/mouse.blade.php
    	new file:   resources/views/components/s-v-g-s/pages.blade.php
    	new file:   resources/views/components/s-v-g-s/settings-2.blade.php
    	new file:   resources/views/components/s-v-g-s/settings-3.blade.php
    	modified:   resources/views/partials/__sidebar.blade.php

--- resources/views/components/dashboard/sidebar/partials/__sidebar-fotter.blade.php
+++ resources/views/components/dashboard/sidebar/partials/__sidebar-fotter.blade.php
@@ -2,21 +2,13 @@
     sidebar-bottom-menu>
 
     <x-dashboard.sidebar.item content="" data-tooltip-target="settings">
-        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z">
-            </path>
-        </svg>
+        <x-s-v-g-s.settings-2 class="w-6 h-6" />
         <x-dashboard.tooltip id="settings" content="settings" />
     </x-dashboard.sidebar.item>
 
     <x-dashboard.sidebar.item content="" path="https://flowbite-admin-dashboard.vercel.app/settings/"
         data-tooltip-target="page-settings">
-        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd"
-                d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                clip-rule="evenodd"></path>
-        </svg>
+        <x-s-v-g-s.settings-3 class="w-6 h-6" />
         <x-dashboard.tooltip id="page-settings" content="Page Settings" />
     </x-dashboard.sidebar.item>
     @include('components.dashboard.sidebar.partials.__sidebar-countrys-dropdown')
